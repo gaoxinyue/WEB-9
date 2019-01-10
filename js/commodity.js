070bef3bc72e7f78952da702b7e8ffa0f1c2beda
gaoxinyue <1119021686@qq.com>
2019-01-10 19:46:05 +0800

    add

--- js/commodity.js
+++ js/commodity.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 
+	// 页面向下滑到100px后，上方的搜索框固定定位
 	var winScro = document.getElementsByClassName("winScro")[0];
 	window.onscroll = function(){
 	    var st = document.documentElement.scrollTop || document.body.scrollTop;
@@ -11,31 +12,30 @@ window.onload = function() {
 	        winScro.style.position = "static";
 	    }
 	}
+	// 页面向下滑结束
 
-/**************/
-
+	// 放大镜，选择下方的小图片或箭头更改img1与img2
 	var box = document.getElementById("box");
 	var img1 = document.getElementById("img1");
 	var slider = document.getElementById("slider");
 	var img2 = document.getElementById("img2");
 	var bimg = document.getElementById("Bimg");
 
-	var text = document.getElementsByClassName("commodity-left-top-text")[0]; 
 	var choiceimage1 = document.getElementsByClassName("choice-image1")[0];
 	var choiceimage2 = document.getElementsByClassName("choice-image2")[0];
-	var image11 = document.getElementsByClassName("image11")[0];
-	var image12 = document.getElementsByClassName("image12")[0];
+	var leftSrow = document.getElementsByClassName("left-srow")[0];
+	var rightSrow = document.getElementsByClassName("right-srow")[0];
 
 	img1.onmouseover = function() {
 		slider.style.display = "block";
+		img1.style.border = "1px solid #676660";
 		img2.style.display = "block";
-		text.style.display = "none";
 	}
 
 	img1.onmouseout = function() {
 		slider.style.display = "none";
+		img1.style.border = "none";
 		img2.style.display = "none";
-		text.style.display = "block";
 	}
 
 	img1.onmousemove = function(e) {
@@ -64,21 +64,24 @@ window.onload = function() {
 
 
 	choiceimage1.onmouseover = function() {
-		image11.style.display = "block";
-		image12.style.display = "none";
+		img1.firstElementChild.src = "../images/pp0.jpeg";
 		bimg.src = "../images/pp0.jpeg";
 	}
 	choiceimage2.onmouseover = function() {
-		image11.style.display = "none";
-		image12.style.display = "block";
+		img1.firstElementChild.src = "../images/pp1.jpeg";
 		bimg.src = "../images/pp1.jpeg";
 	}
+	leftSrow.onclick = function() {
+		img1.firstElementChild.src = "../images/pp0.jpeg";
+		bimg.src = "../images/pp0.jpeg";
+	}
+	rightSrow.onclick = function() {
+		img1.firstElementChild.src = "../images/pp1.jpeg";
+		bimg.src = "../images/pp1.jpeg";
+	}
+	// 放大镜结束
 
-
-
-
-/**************/
-
+	// 选择不同的净含量按钮，更改按钮形态以及下方文字
 	var button1 = document.getElementsByClassName("button1")[0];
 	var button2 = document.getElementsByClassName("button2")[0];
 	var haosheng = document.getElementsByClassName("haosheng")[0];
@@ -96,13 +99,12 @@ window.onload = function() {
 		button2.style.background = "url(../images/duigou.png) no-repeat bottom right";
 		haosheng.innerHTML = '"'+ parseInt(button2.value) + "ml" + '"';
 	}
+	// 选择不同的净含量结束
 
-
-/****************/
+	// 选择数量改变加、减按钮的状态以及数量按钮中的内容
 	var jian = document.getElementsByClassName("jian")[0];
 	var num = document.getElementsByClassName("num")[0];
 	var jia = document.getElementsByClassName("jia")[0];
-	// var jia = document.getElementsByClassName("jia")[0];
 	jian.onclick = function(){
 		if(num.value > 1){
 			num.value = num.value - 1;
@@ -110,6 +112,7 @@ window.onload = function() {
 			jia.style.cursor = "pointer";
 		} else if(num.value == 1){
 			jian.style.cursor = "not-allowed";
+			jia.style.cursor = "pointer";
 		} else{
 			jian.style.cursor = "pointer";
 			jia.style.cursor = "pointer";
@@ -121,14 +124,16 @@ window.onload = function() {
 			jian.style.cursor = "pointer";
 			jia.style.cursor = "pointer";
 		} else if(num.value == 5){
+			jian.style.cursor = "pointer";
 			jia.style.cursor = "not-allowed";
 		} else{
 			jian.style.cursor = "pointer";
 			jia.style.cursor = "pointer";
 		} 
 	}
+	// 选择数量结束
 
-// ******
+	// 点击加入购物车，出现蒙罩，点击选项退出蒙罩
 	var jiarugouwuche = document.getElementsByClassName("jiarugouwuche")[0];
 	var Mengzhao = document.getElementsByClassName("Mengzhao")[0];
 	var chance = document.getElementsByClassName("chance")[0];
@@ -147,6 +152,5 @@ window.onload = function() {
 		Mengzhao.style.display = "none";
 		chance.style.display = "none";
 	}
+	// 点击加入购物车结束
 }
-
-// ******
--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+
+	// 页面向下滑到100px后，上方的搜索框固定定位
+	var winScro = document.getElementsByClassName("winScro")[0];
+	var w12 = document.getElementsByClassName("w12")[0];
+	window.onscroll = function(){
+	    var st = document.documentElement.scrollTop || document.body.scrollTop;
+	    if(st > 100){
+	        winScro.style.position = "fixed";
+	        winScro.style.left = "0";
+	        winScro.style.top = "0";
+	        w12.style.zIndex = "3";
+	    } else {
+	        winScro.style.position = "static";
+	    }
+	}
+	// 页面向下滑结束
+
+	// 大轮播图开始
 	function getStyle(obj, attr){
 		if(obj.currentStyle){
 			return obj.currentStyle[attr];
@@ -34,94 +53,147 @@
 			}
 		}, 30)
 	}
-window.onload = function() {
 
+	var box = document.getElementById('box');
+	var oNavlist = document.getElementById('nav').children;
+	var slider = document.getElementById('slider');
+	var left = document.getElementById('left');
+	var right = document.getElementById('right');
+	var index = 1;
+	var timer;
+	var isMoving = false;
+	box.onmouseover = function(){
+		animate(left,{opacity:100})
+		animate(right,{opacity:100})
+		clearInterval(timer)
+	}
+	box.onmouseout = function(){
+		animate(left,{opacity:0})
+		animate(right,{opacity:0})
+		timer = setInterval(next, 1000);
+	}
+	right.onclick = next;
+	left.onclick = prev;
+	for( var i=0; i<oNavlist.length; i++ ){
+		(function(i){
+			oNavlist[i].onclick = function(){
+				index = i+1;
+				navmove();
+				animate(slider,{left:-800*index});
+			}
+		})(i);
+	}
+	function next(){
+		if(isMoving){
+			return;
+		}
+		isMoving = true;
+		index++;
+		navmove();
+		animate(slider,{left:-800*index},function(){
+			if(index==7){
+				slider.style.left = '-800px';
+				index = 1;
+			}
+			isMoving = false;
+		});
+	}
+	function prev(){
+		if(isMoving){
+			return;
+		}
+		isMoving = true;
+		index--;
+		navmove();
+		animate(slider,{left:-800*index},function(){
+			if(index==0){
+				slider.style.left = '-4800px';
+				index = 6;
+			}
+			isMoving = false;
+		});
+	}
+	function navmove(){
+		for( var i=0; i<oNavlist.length; i++ ){
+			oNavlist[i].className = "";
+		}
+		if(index >6 ){
+			oNavlist[0].className = "active";
+		}else if(index<=0){
+			oNavlist[5].className = "active";
+		}else {
+			oNavlist[index-1].className = "active";
+		}
+	}
+	timer = setInterval(next, 1000);
+	// 大轮播图结束
 
-
-/*****/
+	// 小轮播图开始
 	var slider2 = document.getElementById("slider2");
 	slider2.style.top = "0";
 	slider2.onmouseout = function(){
-		time = setInterval(animate2, 30);
+		time = setInterval(animate2, 20);
 	}
 	slider2.onmouseover = function(){
 		clearInterval(time);
 	}
 
 	function animate2(){
-		var cur = parseInt(slider2.style.top) - 1;
-		slider2.style.top = cur + "px";
-		if(parseInt(slider2.style.top) < -364){ /* 28px*13 */
+		var now = parseInt(slider2.style.top);
+		if(parseInt(slider2.style.top) === -364){ /* 28px*13 */
 			slider2.style.top = "0px";
+		} else {
+			var cur = now - 1;
+			slider2.style.top = cur + "px";
 		}
 	}
-	var time = setInterval(animate2, 30);
-/******/
+	var time = setInterval(animate2, 20);
+	// 小轮播图结束
 
-/********/
-
-	var winScro = document.getElementsByClassName("winScro")[0];
-	window.onscroll = function(){
-	    var st = document.documentElement.scrollTop || document.body.scrollTop;
-	    if(st > 100){
-	        winScro.style.position = "fixed";
-	        winScro.style.left = "0";
-	        winScro.style.top = "0";
-	    } else {
-	        winScro.style.position = "static";
-	    }
-	}
-
-/********/
-
+	// 选择充值数改变文字内容开始
 	var selectNum = document.getElementsByClassName("selectNum")[0];
 	var moneyNum = document.getElementsByClassName("moneyNum")[0];
 	selectNum.onchange = function(){
 	    moneyNum.innerHTML = "￥" + selectNum.value;
 	}
-/********/
+	// 选择充值数结束
 
 
-/********/
 	var w12 = document.getElementsByClassName("w12")[0];
-	w12.onmouseover = function(){
-	    w12.style.right = "0px";
-	}
-	w12.onmouseout = function(){
-	    w12.style.right = "-86px";
-	}
-
-
 	var w3 = document.getElementsByClassName("w3")[0];
-	w3.onmouseover = function(){
-	    w3.style.right = "0px";
-	}
-	w3.onmouseout = function(){
-	    w3.style.right = "-86px";
-	}
-
 	var w45 = document.getElementsByClassName("w45")[0];
 	var w5 = document.getElementsByClassName("w5")[0];
 	var er1 = document.getElementsByClassName("er1")[0];
 	var er2 = document.getElementsByClassName("er2")[0];
+	var w6 = document.getElementsByClassName("w6")[0];
 	er2.style.display = "none";
+	
+	w12.onmouseover = function(){
+	    animate(w12, {right:0});
+	}
+	w12.onmouseout = function(){
+	    animate(w12, {right:-86});
+	}
+	w3.onmouseover = function(){
+	    animate(w3, {right:0});
+	}
+	w3.onmouseout = function(){
+	    animate(w3, {right:-86});
+	}
 	w45.onmouseover = function(){
-	    w45.style.right = "0px";
 	    er1.style.display = "none";
 	    er2.style.display = "block";
+	    animate(w45, {right:0});
 	}
 	w45.onmouseout = function(){
-	    w45.style.right = "-86px";
 	    er1.style.display = "block";
 	    er2.style.display = "none";
+	    animate(w45, {right:-86});
 	}
-
-	var w6 = document.getElementsByClassName("w6")[0];
 	w6.onmouseover = function(){
-	    w6.style.right = "0px";
+	    animate(w6, {right:0});
 	}
 	w6.onmouseout = function(){
-	    w6.style.right = "-86px";
+	    animate(w6, {right:-86});
 	}
 }
-/********/
